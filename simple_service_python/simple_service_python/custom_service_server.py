0080ce517da50026b3fa5b2b1282cea497ff3321
LeeCheahyun <yuipo190@gmail.com>
2022-01-12 11:08:12 +0900

    커밋

--- simple_service_python/simple_service_python/custom_service_server.py
+++ simple_service_python/simple_service_python/custom_service_server.py
@@ -1,48 +1,33 @@
-import sys
 import rclpy
 from rclpy.node import Node
 
 from basic_interface.srv import MockSensorControl
 
-class CustomServiceClient(Node):
+
+class CustomServiceServer(Node):
 
     def __init__(self):
-        super().__init__('custom_service_client')
-        self.client = self.create_client(MockSensorControl, 'mock_sensor_control') # Client builder 패턴
-        while not self.client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Waiting for custom service server...')
-        self.req = MockSensorControl.Request()
+        super().__init__('custom_service_server')
+        self.srv = self.create_service(MockSensorControl, 'mock_sensor_control', self.service_callback)
 
-    def send_request(self):
-        self.req.sensor_id = sys.argv[1]
-        self.req.command = int(sys.argv[2])
-        self.future = self.client.call_async(self.req)
+    def service_callback(self, request, response):
+        if request.command == 1:
+            self.get_logger().info('Control "%s": %d' % (request.sensor_id, request.command))
+            response.is_success = True
+        else:
+            self.get_logger().info('ERROR: unknown command!')
+            response.is_success = False
+            
+        return response
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    custom_service_client = CustomServiceClient()
-    custom_service_client.send_request()
+    custom_service_server = CustomServiceServer()
 
-    while rclpy.ok():
-        rclpy.spin_once(custom_service_client)
-        # future가 done 상태가 되면 다음 if문을 실행합니다
-        if custom_service_client.future.done():
-            try:
-                response = custom_service_client.future.result()
-            except Exception as e:
-                custom_service_client.get_logger().info(
-                    'Service call failed: %r' % (e,))
-            else:
-                if response.is_success:
-                    custom_service_client.get_logger().info('Success(%s)' % custom_service_client.req.sensor_id)
-                else:
-                    custom_service_client.get_logger().info('Failed(%s)' % custom_service_client.req.sensor_id)
+    rclpy.spin(custom_service_server)
 
-                break
-
-    custom_service_client.destroy_node()
     rclpy.shutdown()
 
 
