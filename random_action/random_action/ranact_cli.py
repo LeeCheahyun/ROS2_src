d4710c33adcaa4003e1f8d5ca65bcd118b0c8d4f
LeeCheahyun <yuipo190@gmail.com>
2022-01-13 10:17:16 +0900

    액션

--- random_action/random_action/ranact_cli.py
+++ random_action/random_action/ranact_cli.py
@@ -1,50 +1,45 @@
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
-
 from basic_interface.action import Random
 
-
-class RandomiActionClient(Node):
-
+class Randomactionclient(Node):
     def __init__(self):
         super().__init__('random_action_client')
-        self._action_client = ActionClient(self, Random, 'random')
+        self.action_client = ActionClient(self,Random,'random')
 
-    def send_goal(self, order):
+    def send_goal(self,order):
         goal_msg = Random.Goal()
         goal_msg.order = order
 
-        self._action_client.wait_for_server()
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
+        self.action_client.wait_for_server()
+        self.send_goal_future = self.action_client.send_goal_async(goal_msg,feedback_callback=self.feedback_callback)
+        self.send_goal_future.add_done_callback(self.goal_response_callback)
 
-    def goal_response_callback(self, future):
-        goal_handle = future.result() # Goal Service에서의 response
+    def goal_response_callback(self,future):
+        goal_handle = future.result()
         if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected :(')
+            self.get_logger().info('Goal rejected :')
             return
+        self.get_logger().info('Goal accepted')
+        self.get_result_future = goal_handle.get_result_async()
+        self.get_result_future.add_done_callback(self.get_result_callback)
 
-        self.get_logger().info('Goal accepted :)')
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
-
-    def get_result_callback(self, future):
+    def get_result_callback(self,future):
         result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result.sequence))
-        rclpy.shutdown()
+        if result.sequence == 5:
+            self.get_logger().info('Result: {0}'.format(result.sequence))
+            rclpy.shutdown()
 
-    def feedback_callback(self, feedback_msg):
+    def feedback_callback(self,feedback_msg):
         feedback = feedback_msg.feedback
         self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
 
-
 def main(args=None):
     rclpy.init(args=args)
-    action_client = RandomiActionClient()
-    action_client.send_goal(10)
+    action_client = Randomactionclient()
+    action_client.send_goal(5)
     rclpy.spin(action_client)
-
 
 if __name__ == '__main__':
     main()
--- random_action/random_action/ranact_ser.py
+++ random_action/random_action/ranact_ser.py
@@ -1,51 +1,37 @@
-import time
-
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
-
 from basic_interface.action import Random
+import random
+import time
 
-
-
-class RandomActionServer(Node):
-
+class Randomactionserver(Node):
     def __init__(self):
-        super().__init__('random_action_server')
-        self._action_server = ActionServer(
-            self,
-            Random,
-            'random',
-            self.execute_callback)
+        super().__init__('random_server')
+        self.action_server = ActionServer(self,Random,'random',self.execute_callback)
 
-    def execute_callback(self, goal_handle):
-        self.get_logger().info('Executing goal...')
-
-
+    def execute_callback(self,goal_handle):
+        self.get_logger().info('executing goal,,')
         feedback_msg = Random.Feedback()
-        feedback_msg.partial_sequence = [0, 1]
 
-
-        for i in range(1, goal_handle.request.order):
-            feedback_msg.partial_sequence.append(
-                feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i-1]) # 피보나치 리스트에 데이터 추가
-            self.get_logger().info('Feedback: {0}'.format(feedback_msg.partial_sequence))
+        while True:
+            feedback_msg.partial_sequence = random.randrange(1,11)
+            self.get_logger().info('Feedback : {0}'.format(feedback_msg.partial_sequence))
             goal_handle.publish_feedback(feedback_msg)
             time.sleep(1)
 
+            if feedback_msg.partial_sequence == goal_handle.request.order:
+                break
 
         goal_handle.succeed()
         result = Random.Result()
         result.sequence = feedback_msg.partial_sequence
-
         return result
-
 
 def main(args=None):
     rclpy.init(args=args)
-    fibonacci_action_server = RandomActionServer()
-    rclpy.spin(fibonacci_action_server)
-
+    random_action_server = Randomactionserver()
+    rclpy.spin(random_action_server)
 
 if __name__ == '__main__':
     main()
